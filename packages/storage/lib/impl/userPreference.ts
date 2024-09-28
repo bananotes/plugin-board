0e60fb49177805696ff55cfe9b27d4969d49e66a
Brou <NightKnightBrou@foxmail.com>
2024-09-28 17:00:26 -0400

    feat(popup, storage) @userPreference storage in popup

---
+++ packages/storage/lib/impl/userPreference.ts
@@ -0,0 +1,39 @@
+import { StorageEnum } from '../base/enums';
+import { createStorage } from '../base/base';
+import type { BaseStorage } from '../base/types';
+
+type PopupSettings = {
+  progress: number;
+  isMultiUser: boolean;
+};
+
+type PopupSettingsStorage = BaseStorage<PopupSettings> & {
+  setProgress: (value: number) => Promise<void>;
+  setIsMultiUser: (value: boolean) => Promise<void>;
+};
+
+const defaultSettings: PopupSettings = {
+  progress: 0,
+  isMultiUser: false,
+};
+
+const storage = createStorage<PopupSettings>('popup-settings-storage-key', defaultSettings, {
+  storageEnum: StorageEnum.Local,
+  liveUpdate: true,
+});
+
+export const popupSettingsStorage: PopupSettingsStorage = {
+  ...storage,
+  setProgress: async (value: number) => {
+    await storage.set(currentSettings => ({
+      ...currentSettings,
+      progress: value,
+    }));
+  },
+  setIsMultiUser: async (value: boolean) => {
+    await storage.set(currentSettings => ({
+      ...currentSettings,
+      isMultiUser: value,
+    }));
+  },
+};
