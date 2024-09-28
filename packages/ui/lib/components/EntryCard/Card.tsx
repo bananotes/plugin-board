0a8d9c36aaf5f49640290d10364291da83e0644f
Liuigi <glbrt.liu@gmail.com>
2024-09-28 14:37:12 -0400

    feat(apis): initial version of apis

--- packages/ui/lib/components/EntryCard/Card.tsx
+++ packages/ui/lib/components/EntryCard/Card.tsx
@@ -1,19 +1,18 @@
-import type { ComponentPropsWithoutRef } from 'react';
+import { useEffect, type ComponentPropsWithoutRef } from 'react';
 import { cn } from '../../utils';
+import type { Entry } from '@apis/models';
 
 export type CardProps = {
-  theme?: 'light' | 'dark';
-} & ComponentPropsWithoutRef<'button'>;
+  isEditing: boolean;
+} & Entry &
+  ComponentPropsWithoutRef<'button'>;
 
-export function EntryCard({ theme, className, children, ...props }: CardProps) {
+export function EntryCard({ id: uuid, className, children, ...props }: CardProps) {
+  useEffect(() => {
+    console.log('uuid: ', uuid);
+  }, []);
   return (
-    <button
-      className={cn(
-        className,
-        'py-1 px-4 rounded shadow hover:scale-105',
-        theme === 'light' ? 'bg-white text-black' : 'bg-black text-white',
-      )}
-      {...props}>
+    <button className={cn(className, 'py-1 px-4 rounded shadow hover:scale-105', 'bg-white text-black')} {...props}>
       {children}
     </button>
   );
