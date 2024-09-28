645ea9d890772cc4823d952197b1221920dfb6ee
Liuigi <glbrt.liu@gmail.com>
2024-09-28 03:57:24 -0400

    feat: adjust i18n and content script

--- pages/content-runtime/src/App.tsx
+++ pages/content-runtime/src/App.tsx
@@ -5,5 +5,23 @@ export default function App() {
     console.log('runtime content view loaded');
   }, []);
 
-  return <div className="runtime-content-view-text">runtime content view</div>;
+  const handleClick = () => {
+    console.log('click');
+  };
+  const handleA11yKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter' && e.ctrlKey) {
+      console.log('keydown');
+    }
+  };
+
+  return (
+    <div
+      className="runtime-content-view-text"
+      onClick={handleClick}
+      onKeyDown={handleA11yKeyDown}
+      role="button"
+      tabIndex={0}>
+      runtime content view
+    </div>
+  );
 }
