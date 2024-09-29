639e2e9ea1d5dcc896c4dff6c034b8de17e6bab8
Liuigi <glbrt.liu@gmail.com>
2024-09-28 23:51:30 -0400

    feat(runtime): content rendering and button of likes

--- packages/ui/lib/components/EntryCard/Content.tsx
+++ packages/ui/lib/components/EntryCard/Content.tsx
@@ -1,15 +1,55 @@
-import type { ComponentPropsWithoutRef } from 'react';
+import { useEffect, useState, type ComponentPropsWithoutRef } from 'react';
 import { cn } from '../../utils';
+import { type ContentList } from './env';
 
-export type ButtonProps = ComponentPropsWithoutRef<'button'>;
+export type ContentProps = {
+  content: string;
+} & ComponentPropsWithoutRef<'div'>;
 
-export function Content({ className, children, ...props }: ButtonProps) {
+export function Content({ content, className, ...props }: ContentProps) {
+  const [contentList, setContentList] = useState<ContentList>([
+    {
+      type: 'paragraph',
+      children: [{ text: '' }],
+    },
+  ]);
+  const [showParseError, setShowParseError] = useState(false);
+  useEffect(() => {
+    let rawList = [];
+    try {
+      rawList = JSON.parse(content || '[]');
+    } catch (e) {
+      console.error(e);
+      setShowParseError(true);
+      throw e;
+    }
+    if (Array.isArray(rawList) && rawList.length > 0) {
+      setContentList(rawList);
+    }
+  }, []);
   return (
-    <button
-      className={cn(className, 'cursor-pointer py-1 px-2 mx-1 text-xs rounded shadow hover:scale-105')}
-      {...props}
-      onDragCapture={e => e.stopPropagation}>
-      {children}
-    </button>
+    <div className={cn(className, 'py-2 px-2')} {...props}>
+      {showParseError && <div className="text-red-600 font-l">Failed to parse content</div>}
+      {contentList.map((item, idx) => {
+        return (
+          <div key={idx} className="mb-10">
+            {item.children.map((child, idx) => {
+              return (
+                <p
+                  key={idx}
+                  className={cn(
+                    child.bold && 'font-bold',
+                    child.italic && 'italic',
+                    child.code && 'font-mono',
+                    child.underline && 'underline',
+                  )}>
+                  {child.text}
+                </p>
+              );
+            })}
+          </div>
+        );
+      })}
+    </div>
   );
 }
